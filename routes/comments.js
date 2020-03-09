8146320a3950eecc0f9aa9bb97680efd9ddbd98e
Shirley Shi <shirleyshi0@users.noreply.github.com>
2020-03-09 23:57:40 +1100

    Add authorisation to edit/delete comment

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,7 @@ const express = require('express'),
     Campground = require('../models/campground'),
     Comment = require('../models/comment');
 
-// COMMENTS NEW
+// NEW
 router.get("/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) {
@@ -15,7 +15,7 @@ router.get("/new", isLoggedIn, (req, res) => {
     });
 });
 
-// COMMENTS CREATE
+// CREATE
 router.post("/", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) {
@@ -42,6 +42,57 @@ router.post("/", isLoggedIn, (req, res) => {
     });
 });
 
+// EDIT
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        }
+        else {
+            res.render("comments/edit", {comment: foundComment, campground_id: req.params.id});
+        }
+    });
+});
+
+// UPDATE
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        }
+        else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// DELETE
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+    Comment.findByIdAndDelete(req.params.comment_id, (err) => {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        }
+        else {
+            Campground.findByIdAndUpdate(req.params.id, {
+                $pull: {
+                    comments: req.params.comment_id
+                }
+            }, (err, data) => {
+                if(err) {
+                    console.log(err);
+                    res.redirect("back");
+                }
+                else {
+                    res.redirect("/campgrounds/" + req.params.id);
+                }
+            });
+        }
+    });
+});
+
 //Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
@@ -49,6 +100,25 @@ function isLoggedIn(req, res, next) {
     } 
     else {
         res.redirect("/login");
+    }
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                console.log(err);
+                res.redirect("back");
+            }
+            else {
+                if (foundComment.author.id.equals(req.user.id)) {
+                    next();
+                }
+                else {
+                    res.redirect("back");
+                }
+            }
+        });
     }
 }
 
--- seeds.js
+++ seeds.js
@@ -40,24 +40,24 @@ function seedDB() {
                 else {
                     console.log('removed comments!');
                     //Add campgrounds
-                    data.forEach((seed) => {
-                        Campground.create(seed, (err, campground) => {
-                            if (err) {
-                                console.log(err);
-                            } 
-                            else {
-                                console.log("NEWLY CREATED CAMPGROUND");
-                                // Comment.create({
-                                //     text: "This place is grate, but I wish there was internet",
-                                //     author: "Homer"
-                                // }, (err, comment) => {
-                                //     campground.comments.push(comment);
-                                //     campground.save();
-                                //     console.log("Created new comment");
-                                // });
-                            }
-                        });
-                    });
+                    // data.forEach((seed) => {
+                    //     Campground.create(seed, (err, campground) => {
+                    //         if (err) {
+                    //             console.log(err);
+                    //         } 
+                    //         else {
+                    //             console.log("NEWLY CREATED CAMPGROUND");
+                    //             // Comment.create({
+                    //             //     text: "This place is grate, but I wish there was internet",
+                    //             //     author: "Homer"
+                    //             // }, (err, comment) => {
+                    //             //     campground.comments.push(comment);
+                    //             //     campground.save();
+                    //             //     console.log("Created new comment");
+                    //             // });
+                    //         }
+                    //     });
+                    // });
                 }
             });
         }
